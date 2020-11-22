80c62e11b59691141691ffca5826269d3a8bd906
Kasmanian <46714075+Kasmanian@users.noreply.github.com>
2020-11-22 18:40:24 -0500

    commented out backend server code

--- login/script.js
+++ login/script.js
@@ -1,200 +1,200 @@
 
-const $form = $('#login-form');
-const $message = $('#message');
-const website = "http://localhost:3030/"
-$(function() {
+// const $form = $('#login-form');
+// const $message = $('#message');
+// const website = "http://localhost:3030/"
+// $(function() {
 
     
-    $form.submit(function(e) {
-        e.preventDefault();
+//     $form.submit(function(e) {
+//         e.preventDefault();
   
-        $message.html('');
+//         $message.html('');
 
 
-  //so basically returns input 
-        const dat = $form.serializeArray().reduce((o, x) => {
-            o[x.name] = x.value;
-            //console.log(o)
-            return o;
-        }, {});
-    //so this checks with the user.json by pushing the input into it  
-    //secrets
-        //byid(2)
-        //allid()
-        //add("feli", "foobar")
-        //destroy(2)
-        //login(dat)
-        //edit(1, "foo")
+//   //so basically returns input 
+//         const dat = $form.serializeArray().reduce((o, x) => {
+//             o[x.name] = x.value;
+//             //console.log(o)
+//             return o;
+//         }, {});
+//     //so this checks with the user.json by pushing the input into it  
+//     //secrets
+//         //byid(2)
+//         //allid()
+//         //add("feli", "foobar")
+//         //destroy(2)
+//         //login(dat)
+//         //edit(1, "foo")
         
-    //scores  
-        //byidScore(2)
-    //can use addscore as editScore
-        //addScore("ricky", "hard", 420)
-        //allidScore()
-        //destroyScore(2)
-        //editScore(6, 3000)
-    });
+//     //scores  
+//         //byidScore(2)
+//     //can use addscore as editScore
+//         //addScore("ricky", "hard", 420)
+//         //allidScore()
+//         //destroyScore(2)
+//         //editScore(6, 3000)
+//     });
 
-});
- async function byid(id) {
-    await axios({
-        method: 'get',
-        //so this is how we are doing the postman using these urls
-        url: website + '/secret/'+id,
+// });
+//  async function byid(id) {
+//     await axios({
+//         method: 'get',
+//         //so this is how we are doing the postman using these urls
+//         url: website + '/secret/'+id,
 
-    }).then((resp) => {
-    //console.log(resp.data)
-    return resp.data
-    })
- }
+//     }).then((resp) => {
+//     //console.log(resp.data)
+//     return resp.data
+//     })
+//  }
 
- async function edit(id, pass) {
-    await axios({
-        method: 'put',
-        //so this is how we are doing the postman using these urls
-        url: website + 'secret/'+id,
-        data: {
-            pass: pass,
-        }
+//  async function edit(id, pass) {
+//     await axios({
+//         method: 'put',
+//         //so this is how we are doing the postman using these urls
+//         url: website + 'secret/'+id,
+//         data: {
+//             pass: pass,
+//         }
 
-    }).then((resp) => {
+//     }).then((resp) => {
     
-    return resp.data
-    })
- }
+//     return resp.data
+//     })
+//  }
 
-async function allid() {
-    await axios({
-        method: 'get',
-        //so this is how we are doing the postman using these urls
-        url: website + 'secret',
-    }).then((resp) => {
-        //console.log(resp.data)
-return resp.data
-    })
+// async function allid() {
+//     await axios({
+//         method: 'get',
+//         //so this is how we are doing the postman using these urls
+//         url: website + 'secret',
+//     }).then((resp) => {
+//         //console.log(resp.data)
+// return resp.data
+//     })
  
-}
+// }
 
-async function add(name, password) {
-    await axios({
-        method: 'post',
+// async function add(name, password) {
+//     await axios({
+//         method: 'post',
 
-        url: website + 'secret',
+//         url: website + 'secret',
         
-        data: {
-            name: name,
-            pass: password,
-        }
+//         data: {
+//             name: name,
+//             pass: password,
+//         }
 
-    }).then((resp) => {
-    return resp.data
-    })
- }
+//     }).then((resp) => {
+//     return resp.data
+//     })
+//  }
 
- async function destroy(id) {
-    await axios({
-        method: 'delete',
+//  async function destroy(id) {
+//     await axios({
+//         method: 'delete',
 
-        url:  website + 'secret/' + id,
+//         url:  website + 'secret/' + id,
 
-    }).then((resp) => {
-        return resp.data
-    })
- }
+//     }).then((resp) => {
+//         return resp.data
+//     })
+//  }
 
- async function login(dat) {
-    //console.log(dat.user, dat.password)
+//  async function login(dat) {
+//     //console.log(dat.user, dat.password)
 
-    await axios({
-        method: 'post',
+//     await axios({
+//         method: 'post',
 
-        url: website +  'login',
+//         url: website +  'login',
         
-        data: {
-            name: dat.user,
-            pass: dat.password,
-        }
-    }).then(() => {
-            $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
+//         data: {
+//             name: dat.user,
+//             pass: dat.password,
+//         }
+//     }).then(() => {
+//             $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
 
-            //window.location.replace("/index.html");
+//             //window.location.replace("/index.html");
             
-        }).catch(() => {
+//         }).catch(() => {
             
-            $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your name and password and your internet connection.</span>');
+//             $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your name and password and your internet connection.</span>');
         
              
-        });
- }
+//         });
+//  }
 
-// Turn the data object into an array of URL-encoded key/value pairs.
+// // Turn the data object into an array of URL-encoded key/value pairs.
 
- async function byidScore(id) {
-    await axios({
-        method: 'get',
-        //so this is how we are doing the postman using these urls
-        url:  website +  'score/'+id,
+//  async function byidScore(id) {
+//     await axios({
+//         method: 'get',
+//         //so this is how we are doing the postman using these urls
+//         url:  website +  'score/'+id,
 
-    }).then((resp) => {
-    //console.log(resp.data.name)
-    //console.log(resp.data)
-    return resp.data
-    })
- }
+//     }).then((resp) => {
+//     //console.log(resp.data.name)
+//     //console.log(resp.data)
+//     return resp.data
+//     })
+//  }
 
- async function editScore(id, score) {
-    await axios({
-        method: 'put',
-        //so this is how we are doing the postman using these urls
-        url: website + 'score/'+id,
-        data: {
-            score: score,
-        }
+//  async function editScore(id, score) {
+//     await axios({
+//         method: 'put',
+//         //so this is how we are doing the postman using these urls
+//         url: website + 'score/'+id,
+//         data: {
+//             score: score,
+//         }
 
-    }).then((resp) => {
+//     }).then((resp) => {
     
-    return resp.data
-    })
- }
+//     return resp.data
+//     })
+//  }
 
-async function allidScore() {
-    await axios({
-        method: 'get',
-        //so this is how we are doing the postman using these urls
-        url: website + 'score',
-    }).then((resp) => {
-        //console.log(resp.data)
-return resp.data
-    })
+// async function allidScore() {
+//     await axios({
+//         method: 'get',
+//         //so this is how we are doing the postman using these urls
+//         url: website + 'score',
+//     }).then((resp) => {
+//         //console.log(resp.data)
+// return resp.data
+//     })
  
-}
+// }
 
-async function addScore(name, mode, score) {
-    await axios({
-        method: 'post',
+// async function addScore(name, mode, score) {
+//     await axios({
+//         method: 'post',
 
-        url: website + 'score',
+//         url: website + 'score',
         
-        data: {
-            name: name,
-            mode: mode,
-            high_score: score,
-        }
+//         data: {
+//             name: name,
+//             mode: mode,
+//             high_score: score,
+//         }
 
-    }).then((resp) => {
-    return resp.data
-    })
- }
+//     }).then((resp) => {
+//     return resp.data
+//     })
+//  }
 
- async function destroyScore(id) {
-    await axios({
-        method: 'delete',
+//  async function destroyScore(id) {
+//     await axios({
+//         method: 'delete',
 
-        url: website + 'score/' + id,
+//         url: website + 'score/' + id,
         
 
-    }).then((resp) => {
-        return resp.data
-    })
- }
+//     }).then((resp) => {
+//         return resp.data
+//     })
+//  }
 
--- login/server.js
+++ login/server.js
@@ -1,227 +1,227 @@
 //this calls secret functions
 
-const express = require('express');
-var cors = require('cors')
-const app = express();
-app.use(cors());
+// const express = require('express');
+// var cors = require('cors')
+// const app = express();
+// app.use(cors());
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
 
-const expressSession = require('express-session');
-//creates cookie
-app.use(expressSession({
-    name: "kmpSessionCookie",
-    secret: "express session secret",
-    resave: false,
-    saveUninitialized: false
-}));
+// const expressSession = require('express-session');
+// //creates cookie
+// app.use(expressSession({
+//     name: "kmpSessionCookie",
+//     secret: "express session secret",
+//     resave: false,
+//     saveUninitialized: false
+// }));
 
-const Secret = require("./Secret.js").Secret;
-const Score = require("./Secret.js").Score;
-const login_data = require('data-store')({ path: process.cwd() + '/data/secrets.json' });
-//this checks users
-app.post('/login', (req,res) => {
-    //console.log("hiiiiii")
-    //by name get id
-    let name = req.body.name;
-    id = null
+// const Secret = require("./Secret.js").Secret;
+// const Score = require("./Secret.js").Score;
+// const login_data = require('data-store')({ path: process.cwd() + '/data/secrets.json' });
+// //this checks users
+// app.post('/login', (req,res) => {
+//     //console.log("hiiiiii")
+//     //by name get id
+//     let name = req.body.name;
+//     id = null
     
-    Secret.getAllIDs().forEach(ele => {
-        //console.log(ele)
-        if (ele != undefined ){
-            if(Secret.findByID(ele.toString()).name == name) {
-                //console.log(Secret.findByID(ele.toString()))
-                id= Secret.findByID(ele.toString()).id
+//     Secret.getAllIDs().forEach(ele => {
+//         //console.log(ele)
+//         if (ele != undefined ){
+//             if(Secret.findByID(ele.toString()).name == name) {
+//                 //console.log(Secret.findByID(ele.toString()))
+//                 id= Secret.findByID(ele.toString()).id
                 
-            }
-        }
+//             }
+//         }
        
-    });
+//     });
     
-    if (id == null) {
-        //console.log("its null")
-        res.status(404).send("Username does not exist");
-        return;
-    }
-    let pass = req.body.pass;
-    let user_data = login_data.get(id.toString());
-   // console.log(user_data)
+//     if (id == null) {
+//         //console.log("its null")
+//         res.status(404).send("Username does not exist");
+//         return;
+//     }
+//     let pass = req.body.pass;
+//     let user_data = login_data.get(id.toString());
+//    // console.log(user_data)
 
-    //console.log(user_data.pass, pass)
-    if (user_data.pass == pass) {
-        //console.log("User " + user + " credentials valid");
+//     //console.log(user_data.pass, pass)
+//     if (user_data.pass == pass) {
+//         //console.log("User " + user + " credentials valid");
 
-        //console.log(user)
-        res.json(true);
-        return;
-    }
-    res.status(403).send("Unauthorized");
-});
+//         //console.log(user)
+//         res.json(true);
+//         return;
+//     }
+//     res.status(403).send("Unauthorized");
+// });
 
 
-app.get('/secret', (req, res) => {
+// app.get('/secret', (req, res) => {
     
-    res.json(Secret.getAllIDs())
+//     res.json(Secret.getAllIDs())
 
-     return;
-});
-//find certain account
-app.get('/secret/:id', (req, res) => {
+//      return;
+// });
+// //find certain account
+// app.get('/secret/:id', (req, res) => {
 
-    let s = Secret.findByID(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
+//     let s = Secret.findByID(req.params.id);
+//     if (s == null) {
+//         res.status(404).send("Not found");
+//         return;
+//     }
 
 
 
-    res.json(s);
-} );
-//post are done via postman
+//     res.json(s);
+// } );
+// //post are done via postman
 
-app.post('/secret', (req, res)=> {
-    //same name
-    Secret.getAllIDs().forEach(ele => {
+// app.post('/secret', (req, res)=> {
+//     //same name
+//     Secret.getAllIDs().forEach(ele => {
         
-        if (ele != undefined ){
-            if(Secret.findByID(ele.toString()).name == name) {
-                //console.log(Secret.findByID(ele.toString()))
-                res.status(400).send("Bad Request");
-                return;
+//         if (ele != undefined ){
+//             if(Secret.findByID(ele.toString()).name == name) {
+//                 //console.log(Secret.findByID(ele.toString()))
+//                 res.status(400).send("Bad Request");
+//                 return;
                 
-            }
-        }
+//             }
+//         }
        
-    });
-    let s = Secret.create(req.body.name, req.body.pass);
-    //console.log(s)
-    if (s == null) {
-        res.status(400).send("Bad Request");
-        return;
-    }
-    return res.json(s);
-});
-//edit account
-app.put('/secret/:id', (req, res) => {
+//     });
+//     let s = Secret.create(req.body.name, req.body.pass);
+//     //console.log(s)
+//     if (s == null) {
+//         res.status(400).send("Bad Request");
+//         return;
+//     }
+//     return res.json(s);
+// });
+// //edit account
+// app.put('/secret/:id', (req, res) => {
 
-    let s = Secret.findByID(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-    //console.log(req.body.pass.toString())
-    s.update(req.body.pass);
-    res.json(s.id);
-});
+//     let s = Secret.findByID(req.params.id);
+//     if (s == null) {
+//         res.status(404).send("Not found");
+//         return;
+//     }
+//     //console.log(req.body.pass.toString())
+//     s.update(req.body.pass);
+//     res.json(s.id);
+// });
 
-app.delete('/secret/:id', (req, res) => {
+// app.delete('/secret/:id', (req, res) => {
 
-    let s = Secret.findByID(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-    s.delete();
-    res.json(true);
-})
-
-
+//     let s = Secret.findByID(req.params.id);
+//     if (s == null) {
+//         res.status(404).send("Not found");
+//         return;
+//     }
+//     s.delete();
+//     res.json(true);
+// })
 
 
-//heroku makes its own portnum cant hardcode
-const port = 3030;
-app.listen(port, () => {
-    console.log("User Login Example up and running on port " + port);
-});
 
 
+// //heroku makes its own portnum cant hardcode
+// const port = 3030;
+// app.listen(port, () => {
+//     console.log("User Login Example up and running on port " + port);
+// });
 
 
 
 
 
-const score_data = require('data-store')({ path: process.cwd() + '/data/scores.json' });
-
-//this checks users
-
-app.get('/score', (req, res) => {
-    res.json(Score.getAllIDs2())
-
-     return;
-});
-//find certain account
-app.get('/score/:id', (req, res) => {
-
-    let s = Score.findByID2(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
 
 
+// const score_data = require('data-store')({ path: process.cwd() + '/data/scores.json' });
 
-    res.json(s);
-} );
-//add new
+// //this checks users
 
-app.post('/score', (req, res)=> {
-    //if already exist 
-    var exist = null
-    Score.getAllIDs2().forEach(ele => {
+// app.get('/score', (req, res) => {
+//     res.json(Score.getAllIDs2())
+
+//      return;
+// });
+// //find certain account
+// app.get('/score/:id', (req, res) => {
+
+//     let s = Score.findByID2(req.params.id);
+//     if (s == null) {
+//         res.status(404).send("Not found");
+//         return;
+//     }
+
+
+
+//     res.json(s);
+// } );
+// //add new
+
+// app.post('/score', (req, res)=> {
+//     //if already exist 
+//     var exist = null
+//     Score.getAllIDs2().forEach(ele => {
         
-        if (ele != undefined ){
-            if(Score.findByID2(ele.toString()).name == req.body.name) {
+//         if (ele != undefined ){
+//             if(Score.findByID2(ele.toString()).name == req.body.name) {
                 
-                if(Score.findByID2(ele.toString()).mode == req.body.mode) {
+//                 if(Score.findByID2(ele.toString()).mode == req.body.mode) {
                 
-                    exist = Score.findByID2(ele.toString())
+//                     exist = Score.findByID2(ele.toString())
                     
-                }
+//                 }
                 
-            }
-        }
+//             }
+//         }
        
-    });
+//     });
 
-    if (exist != null) {
-        exist.update2(req.body.high_score);
-        return res.json(exist);
-    }
-
-
-    let s = Score.create2(req.body.name, req.body.mode, req.body.high_score,);
-    //console.log(s)
-    if (s == null) {
-        res.status(400).send("Bad Request");
-        return;
-    }
+//     if (exist != null) {
+//         exist.update2(req.body.high_score);
+//         return res.json(exist);
+//     }
 
 
-    return res.json(s);
-});
-//edit score
-app.put('/score/:id', (req, res) => {
+//     let s = Score.create2(req.body.name, req.body.mode, req.body.high_score,);
+//     //console.log(s)
+//     if (s == null) {
+//         res.status(400).send("Bad Request");
+//         return;
+//     }
 
-    let s = Score.findByID2(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-    //console.log(req.body.pass)
-    s.update2(req.body.score);
-    res.json(s.id);
-});
 
-app.delete('/score/:id', (req, res) => {
+//     return res.json(s);
+// });
+// //edit score
+// app.put('/score/:id', (req, res) => {
 
-    let s = Score.findByID2(req.params.id);
-    if (s == null) {
-        res.status(404).send("Not found");
-        return;
-    }
-    s.delete2();
-    res.json(true);
-})
+//     let s = Score.findByID2(req.params.id);
+//     if (s == null) {
+//         res.status(404).send("Not found");
+//         return;
+//     }
+//     //console.log(req.body.pass)
+//     s.update2(req.body.score);
+//     res.json(s.id);
+// });
+
+// app.delete('/score/:id', (req, res) => {
+
+//     let s = Score.findByID2(req.params.id);
+//     if (s == null) {
+//         res.status(404).send("Not found");
+//         return;
+//     }
+//     s.delete2();
+//     res.json(true);
+// })
 
