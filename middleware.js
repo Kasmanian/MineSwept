8801558c459a0b54bb460440c0cf546541190eac
Kasmanian <46714075+Kasmanian@users.noreply.github.com>
2020-11-22 19:21:03 -0500

    added middleware port

--- middleware.js
+++ middleware.js
@@ -7,4 +7,7 @@ app.use(favicon(path.join('./assets/favicon/favicon-16x16.png', 'public', 'favic
 
 // Add your routes here, etc.
 
-app.listen(3000)
+var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+});
